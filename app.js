92d887904cc5bf0937c750468727fcbaf1f6f527
Paula Leal <paula61395@gmail.com>
2021-09-03 22:35:51 -0400

    edited procfile and app.js

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use('/item', itemRouter);
 app.use('/category', categoryRouter);
 app.use('/newItem', newItemRouter);
 
+app.listen(process.env.PORT || 8000);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
